715fc376d63663e3247b407cd07222f17961890b
Andrew Muto <amuto77@gmail.com>
2020-07-17 13:27:09 -0400

    Completed JS Skeleton for Character Creation

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,14 @@
 // Write your Character component here
+import React from 'react';
+import CharacterCard from "./CharacterCard";
+
+function Character(props) {
+    return (
+        <>
+        {props.character.map((character) => {
+            return <CharacterCard character={character} key={character.id} />
+        })}
+        </>
+    );
+}
+export default Character;
